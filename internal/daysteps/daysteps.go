e14bde86e860c903f015a6955d48c09489269e88
Asfode1 <scorp5978@inbox.ru>
2025-05-09 09:38:06 +0300

    Функция parsePackage()

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -2,6 +2,9 @@ package daysteps
 
 import (
 	"time"
+	"errors"
+	"strconv"
+	"strings" 
 )
 
 const (
@@ -12,7 +15,19 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	parts := strings.Split(data, ",")
+	if len(parts) != 2 {
+		return 0, 0, errors.New("неверный формат входных данных: должна быть одна запятая между шагами и продолжительностью")
+	}
+
+	// Парсим количество шагов
+	steps, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return 0, 0, errors.New("ошибка при разборе количества шагов: " + err.Error())
+	}
+	if steps <= 0 {
+		return 0, 0, errors.New("количество шагов должно быть больше нуля")
+	}
 }
 
 func DayActionInfo(data string, weight, height float64) string {
