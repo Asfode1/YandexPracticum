445e31a0d0666d6dcfc840544b0155fabc6a57e8
Asfode1 <scorp5978@inbox.ru>
2025-05-09 11:33:03 +0300

    реализовал 2 функции в пакете daysteps

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -4,7 +4,10 @@ import (
 	"time"
 	"errors"
 	"strconv"
-	"strings" 
+	"strings"
+	"fmt"
+	"log"
+	
 )
 
 const (
@@ -28,8 +31,37 @@ func parsePackage(data string) (int, time.Duration, error) {
 	if steps <= 0 {
 		return 0, 0, errors.New("количество шагов должно быть больше нуля")
 	}
-}
+	duration, err := time.ParseDuration(parts[1])
+	if err != nil {
+		return 0, 0, fmt.Errorf("ошибка при разборе продолжительности: %v", err)
+	}
 
+	return steps, duration, nil
+}
+// DayActionInfo парсит данные, вычисляет дистанцию и калории, возвращает форматированную строку
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	steps, duration, err := parsePackage(data)
+	if err != nil {
+		log.Println("Ошибка парсинга данных:", err)
+		return ""
+	}
+
+	if steps <= 0 {
+		return ""
+	}
+	// Вычисляем дистанцию в метрах
+	distanceMeters := float64(steps) * stepLength
+	// Переводим в километры
+	distanceKm := distanceMeters / mInKm
+
+	// Вычисляем калории
+	calories := spentcalories.WalkingSpentCalories(weight, height, duration)
+
+	// Формируем строку результата
+	result := fmt.Sprintf(
+		"Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.",
+		steps, distanceKm, calories,
+	)
+
+	return result
 }
